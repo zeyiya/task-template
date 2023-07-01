62765e97dc75367432b218cf6cfb230bbf279f4e
qq <qq11283020392@protonmail.com>
2023-07-01 09:29:22 +0800

    qq

--- coreLogic.js
+++ coreLogic.js
@@ -4,23 +4,7 @@ const crypto = require('crypto');
 class CoreLogic {
   async task() {
     // Write the logic to do the work required for submitting the values and optionally store the result in levelDB
-
-    // Below is just a sample of work that a task can do
-
-    try {
-      const x = Math.random().toString(); // generate random number and convert to string
-      const cid = crypto.createHash('sha1').update(x).digest('hex'); // convert to CID
-      console.log('HASH:', cid);
-      // fetching round number to store work accordingly
-
-      if (cid) {
-        await namespaceWrapper.storeSet('cid', cid); // store CID in levelDB
-      }
-      return cid
-    } catch (err) {
-      console.log('ERROR IN EXECUTING TASK', err);
-      return 'ERROR IN EXECUTING TASK' + err
-    }
+    console.log('...')
   }
   async fetchSubmission() {
     // Write the logic to fetch the submission values here and return the cid string
@@ -31,8 +15,7 @@ class CoreLogic {
 
     // The code below shows how you can fetch your stored value from level DB
 
-    const cid = await namespaceWrapper.storeGet('cid'); // retrieves the cid
-    console.log('CID', cid);
+    const sub = "大我大";
     return cid;
   }
 
@@ -46,82 +29,11 @@ class CoreLogic {
       /*  **** SAMPLE LOGIC FOR GENERATING DISTRIBUTION LIST ******/
 
       let distributionList = {};
-      let distributionCandidates = [];
-      let taskAccountDataJSON = await namespaceWrapper.getTaskState();
-      if (taskAccountDataJSON == null) taskAccountDataJSON = _dummyTaskState;
-      const submissions = taskAccountDataJSON.submissions[round];
-      const submissions_audit_trigger =
-        taskAccountDataJSON.submissions_audit_trigger[round];
-      if (submissions == null) {
-        console.log('No submisssions found in N-2 round');
-        return distributionList;
-      } else {
-        const keys = Object.keys(submissions);
-        const values = Object.values(submissions);
-        const size = values.length;
-        console.log('Submissions from last round: ', keys, values, size);
-
-        // Logic for slashing the stake of the candidate who has been audited and found to be false
-        for (let i = 0; i < size; i++) {
-          const candidatePublicKey = keys[i];
-          if (
-            submissions_audit_trigger &&
-            submissions_audit_trigger[candidatePublicKey]
-          ) {
-            console.log(
-              'distributions_audit_trigger votes ',
-              submissions_audit_trigger[candidatePublicKey].votes,
-            );
-            const votes = submissions_audit_trigger[candidatePublicKey].votes;
-            if (votes.length === 0) {
-              // slash 70% of the stake as still the audit is triggered but no votes are casted
-              // Note that the votes are on the basis of the submission value
-              // to do so we need to fetch the stakes of the candidate from the task state
-              const stake_list = taskAccountDataJSON.stake_list;
-              const candidateStake = stake_list[candidatePublicKey];
-              const slashedStake = candidateStake * 0.7;
-              distributionList[candidatePublicKey] = -slashedStake;
-              console.log('Candidate Stake', candidateStake);
-            } else {
-              let numOfVotes = 0;
-              for (let index = 0; index < votes.length; index++) {
-                if (votes[index].is_valid) numOfVotes++;
-                else numOfVotes--;
-              }
-
-              if (numOfVotes < 0) {
-                // slash 70% of the stake as the number of false votes are more than the number of true votes
-                // Note that the votes are on the basis of the submission value
-                // to do so we need to fetch the stakes of the candidate from the task state
-                const stake_list = taskAccountDataJSON.stake_list;
-                const candidateStake = stake_list[candidatePublicKey];
-                const slashedStake = candidateStake * 0.7;
-                distributionList[candidatePublicKey] = -slashedStake;
-                console.log('Candidate Stake', candidateStake);
-              }
-
-              if (numOfVotes > 0) {
-                distributionCandidates.push(candidatePublicKey);
-              }
-            }
-          } else {
-            distributionCandidates.push(candidatePublicKey);
-          }
-        }
-      }
-
       // now distribute the rewards based on the valid submissions
       // Here it is assumed that all the nodes doing valid submission gets the same reward
 
-      const reward =
-        taskAccountDataJSON.bounty_amount_per_round /
-        distributionCandidates.length;
-      console.log('REWARD RECEIVED BY EACH NODE', reward);
-      for (let i = 0; i < distributionCandidates.length; i++) {
-        distributionList[distributionCandidates[i]] = reward;
-      }
-      console.log('Distribution List', distributionList);
       return distributionList;
+
     } catch (err) {
       console.log('ERROR IN GENERATING DISTRIBUTION LIST', err);
     }
